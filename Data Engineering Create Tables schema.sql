9bf27bd7ec3a833480fe3a5af65a7c9d16fb438b
sanaz shahbazian <58539808+sanazshahbazian@users.noreply.github.com>
2020-03-09 16:17:16 -0700

    Add files via upload

--- Data Engineering Create Tables schema.sql
+++ Data Engineering Create Tables schema.sql
@@ -68,3 +68,11 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
+-- Query * FROM Each Table Confirming Data
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
